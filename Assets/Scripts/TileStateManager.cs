c2dadf7b77131afe98f33d12a3036d01e7396de9
Mtrebi <marianotrebino@gmail.com>
2017-06-19 14:29:25 +0200

    Added automatic rotations

--- Assets/Scripts/TileStateManager.cs
+++ Assets/Scripts/TileStateManager.cs
@@ -3,40 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TileStateManager : MonoBehaviour {
-  public TileState[] tile_states_;
-  public Texture2D[] tile_textures_;
-
+  public GameObject[] tile_prefabs_;
+  public List<TileState> tile_states_;
   public bool enable_rotations_;
   // TODO public bool enable_reflections_;
 
-
-  public Texture2D GetTexture(int id) {
-    return tile_textures_[id];
-  }
-
   // Use this for initialization
   void Start () {
-    tile_states_ = new TileState[16];
+    tile_states_ = new List<TileState>();
+    
+    TileState ts_0000 = new TileState(new Bin("0000"), tile_prefabs_[0], 0.075f);
+    tile_states_.Add(ts_0000);
+    tile_states_.AddRange(GenerateRotations(ts_0000));
 
-    tile_states_[0] = new TileState(new Bin("0000"), tile_textures_[0], 0.06f);
-    tile_states_[1] = new TileState(new Bin("0001"), tile_textures_[1], 0.07f);
-    tile_states_[2] = new TileState(new Bin("0010"), tile_textures_[2], 0.07f);
-    tile_states_[3] = new TileState(new Bin("0011"), tile_textures_[3], 0.06f);
-    tile_states_[4] = new TileState(new Bin("0100"), tile_textures_[4], 0.065f);
-    tile_states_[5] = new TileState(new Bin("0101"), tile_textures_[5], 0.05f);
-    tile_states_[6] = new TileState(new Bin("0110"), tile_textures_[6], 0.06f);
-    tile_states_[7] = new TileState(new Bin("0111"), tile_textures_[7], 0.06f);
-    tile_states_[8] = new TileState(new Bin("1000"), tile_textures_[8], 0.06f);
-    tile_states_[9] = new TileState(new Bin("1001"), tile_textures_[9], 0.065f);
-    tile_states_[10] = new TileState(new Bin("1010"), tile_textures_[10], 0.06f);
-    tile_states_[11] = new TileState(new Bin("1011"), tile_textures_[11], 0.06f);
-    tile_states_[12] = new TileState(new Bin("1100"), tile_textures_[12], 0.065f);
-    tile_states_[13] = new TileState(new Bin("1101"), tile_textures_[13], 0.07f);
-    tile_states_[14] = new TileState(new Bin("1110"), tile_textures_[14], 0.065f);
-    tile_states_[15] = new TileState(new Bin("1111"), tile_textures_[15], 0.06f);
+    TileState ts_0001 = new TileState(new Bin("0001"), tile_prefabs_[1], 0.05f);
+    tile_states_.Add(ts_0001);
+    tile_states_.AddRange(GenerateRotations(ts_0001));
 
-    if (enable_rotations_) {
-      //GenerateRotations();
-    }
+    TileState ts_0011 = new TileState(new Bin("0011"), tile_prefabs_[2], 0.05f);
+    tile_states_.Add(ts_0011);
+    tile_states_.AddRange(GenerateRotations(ts_0011));
+    
+    TileState ts_0101 = new TileState(new Bin("0101"), tile_prefabs_[3], 0.10f);
+    tile_states_.Add(ts_0101);
+    tile_states_.AddRange(GenerateRotations(ts_0101));
+
+    TileState ts_0111 = new TileState(new Bin("0111"), tile_prefabs_[4], 0.05f);
+    tile_states_.Add(ts_0111);
+    tile_states_.AddRange(GenerateRotations(ts_0111));
+
+    TileState ts_1111 = new TileState(new Bin("1111"), tile_prefabs_[5], 0.075f);
+    tile_states_.Add(ts_1111);
+    tile_states_.AddRange(GenerateRotations(ts_1111));
   }
+
+  private TileState[] GenerateRotations(TileState tile_state) {
+    HashSet<TileState> tile_rotations = new HashSet<TileState>();
+
+    for (int i = 1; i < tile_state.shape_id_.Length(); ++i) {
+      Bin rotation_id = tile_state.shape_id_.Rotate(i);
+      TileState new_state = new TileState(rotation_id, tile_state.prefab_, tile_state.probability_, new Vector3(i * 90, 0, 0));
+      if (!tile_state.Equals(new_state)) {
+        tile_rotations.Add(new_state);
+      }
+    }
+
+    TileState[] tile_rotations_array = new TileState[tile_rotations.Count];
+    tile_rotations.CopyTo(tile_rotations_array);
+
+    return tile_rotations_array;
+  }
+
 }
