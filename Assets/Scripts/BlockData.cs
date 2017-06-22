d63e7ebf1ba1ffc87cb67ee715129a0354cc4e56
Mtrebi <marianotrebino@gmail.com>
2017-06-22 11:24:17 +0200

    Added Block data script to hold id and probability

---
+++ Assets/Scripts/BlockData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Data holder for Block prefabs
+/// </summary>
+public class BlockData : MonoBehaviour {
+  public string id;
+  public float probability;
+}
--- Assets/Scripts/TileStateManager.cs
+++ Assets/Scripts/TileStateManager.cs
@@ -13,25 +13,12 @@ public class TileStateManager : MonoBehaviour {
 
     // TODO get id and probability from game object  
 
-    TileState ts_0000 = new TileState(new Bin("0000"), tile_prefabs_[0], 0.2f);
-    tile_states_.Add(ts_0000);
-    tile_states_.AddRange(GenerateRotations(ts_0000));
-    
-    TileState ts_1000 = new TileState(new Bin("1000"), tile_prefabs_[1], 0.05f);
-    tile_states_.Add(ts_1000);
-    tile_states_.AddRange(GenerateRotations(ts_1000));
-    
-    TileState ts_1100 = new TileState(new Bin("1100"), tile_prefabs_[2], 0.05f);
-    tile_states_.Add(ts_1100);
-    tile_states_.AddRange(GenerateRotations(ts_1100));
-    
-    TileState ts_1110 = new TileState(new Bin("1110"), tile_prefabs_[3], 0.05f);
-    tile_states_.Add(ts_1110);
-    tile_states_.AddRange(GenerateRotations(ts_1110));
-    
-    TileState ts_1111 = new TileState(new Bin("1111"), tile_prefabs_[4], 0.2f);
-    tile_states_.Add(ts_1111);
-    tile_states_.AddRange(GenerateRotations(ts_1111));
+    foreach (GameObject tile_prefab in tile_prefabs_) {
+      BlockData data = tile_prefab.GetComponent<BlockData>();
+      TileState ts = new TileState(new Bin(data.id), tile_prefab, data.probability);
+      tile_states_.Add(ts);
+      tile_states_.AddRange(GenerateRotations(ts));
+    }
   }
 
   private TileState[] GenerateRotations(TileState tile_state) {
@@ -40,7 +27,6 @@ public class TileStateManager : MonoBehaviour {
     for (int i = 1; i < tile_state.shape_id_.Length(); ++i) {
       Bin rotation_id = tile_state.shape_id_.Rotate(i);
       // TODO Check id and model
-      // TODO Generate constraint id rotation (cheaper than generate constraint from scratch)
       if (!rotation_id.Equals(tile_state.shape_id_)) {
         TileState new_state = new TileState(rotation_id, tile_state.prefab_, tile_state.probability_, new Vector3(0, -i * 90, 0));
         tile_rotations.Add(new_state);
