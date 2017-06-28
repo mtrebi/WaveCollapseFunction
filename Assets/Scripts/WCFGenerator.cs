1ed1438eea831856938bf4cc74be5edd61c7f693
Mtrebi <marianotrebino@gmail.com>
2017-06-28 11:41:16 +0200

    Added Y dimension

--- Assets/Scripts/WCFGenerator.cs
+++ Assets/Scripts/WCFGenerator.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class WCFGenerator : MonoBehaviour {
   public int width_,
-             height_;
+             height_,
+             depth_;
   public bool random_start_;
   public GameObject tile_state_manager_object_;
 
-  private Tile[,] wave_;
-  private bool[,] wave_changed_;
+  private Tile[,,] wave_;
+  private bool[,,] wave_changed_;
 
   private bool finished_ = false;
   private bool first_iteration_ = true;
@@ -29,17 +30,19 @@ public class WCFGenerator : MonoBehaviour {
   void Update() {
     if (!finished_) {
       GenerateWave();
-      RenderWave();
+        RenderWave();
     }
   }
 
   private void InitializeWave() {
-    wave_ = new Tile[width_, height_];
-    wave_changed_ = new bool[width_, height_];
+    wave_ = new Tile[width_, height_, depth_];
+    wave_changed_ = new bool[width_, height_, depth_];
     for (int x = 0; x < width_; ++x) {
-      for (int z = 0; z < height_; ++z) {
-        wave_[x, z] = TileFactory.Instance.CreateDefaultTile(this.transform, x, z, new List<TileState>(tile_state_manager_object_.GetComponent<TileStateManager>().tile_states_));
-        wave_changed_[x, z] = true;
+      for (int y = 0; y < height_; ++y) {
+        for (int z = 0; z < depth_; ++z) {
+          wave_[x,y,z] = TileFactory.Instance.CreateDefaultTile(this.transform, x, y, z, new List<TileState>(tile_state_manager_object_.GetComponent<TileStateManager>().tile_states_));
+          wave_changed_[x, y, z] = true;
+        }
       }
     }
   }
@@ -59,9 +62,9 @@ public class WCFGenerator : MonoBehaviour {
   
   private void RenderWave() {
     foreach (Tile tile in wave_) {
-      if (wave_changed_[tile.X, tile.Z]) {
+      if (wave_changed_[tile.X, tile.Y, tile.Z]) {
         tile.Render(this.gameObject.transform);
-        wave_changed_[tile.X, tile.Z] = false;
+        wave_changed_[tile.X, tile.Y, tile.Z] = false;
       }
     }
   }
@@ -74,9 +77,9 @@ public class WCFGenerator : MonoBehaviour {
     if (first_iteration_) {
       first_iteration_ = false;
       if (random_start_) {
-        return wave_[Random.Range(0, width_), Random.Range(0, height_)];
+        return wave_[Random.Range(0, width_), Random.Range(0, height_), Random.Range(0, depth_)];
       }
-      return wave_[0, 0];
+      return wave_[0, 0, 0];
     }      
 
     Tile min_entropy_tile = null;
@@ -84,16 +87,17 @@ public class WCFGenerator : MonoBehaviour {
 
     for (int x = 0; x < width_; ++x) {
       for (int y = 0; y < height_; ++y) {
-        Tile tile = wave_[x, y];
-        if (!tile.Collapsed()) {
-          float current_entropy = tile.GetEntropy();
+        for (int z = 0; z < depth_; ++z) {
+          Tile tile = wave_[x, y, z];
+          if (!tile.Collapsed()) {
+            float current_entropy = tile.GetEntropy();
 
-          if (current_entropy < min_entropy) {
-            min_entropy_tile = tile;
-            min_entropy = current_entropy;
+            if (current_entropy < min_entropy) {
+              min_entropy_tile = tile;
+              min_entropy = current_entropy;
+            }
           }
         }
-
       }
     }
     return min_entropy_tile;
@@ -108,7 +112,7 @@ public class WCFGenerator : MonoBehaviour {
       Tile current_tile = remaining_tiles.Pop();
       Tile[] neighbors = GetNeighbors(current_tile);
 
-      wave_changed_[current_tile.X, current_tile.Z] = true;
+      wave_changed_[current_tile.X, current_tile.Y, current_tile.Z] = true;
 
       foreach (Tile neighbor in neighbors) {
         if (!neighbor.Collapsed() 
@@ -125,15 +129,20 @@ public class WCFGenerator : MonoBehaviour {
     if (tile.X > neighbor.X) {
       return Direction.NORTH;
     }
-
-    if (tile.Z < neighbor.Z) {
-      return Direction.EAST;
-    }
-
     if (tile.X < neighbor.X) {
       return Direction.SOUTH;
     }
 
+    if (tile.Y < neighbor.Y) {
+      return Direction.TOP;
+    }
+    if (tile.Y > neighbor.Y) {
+      return Direction.BOTTOM;
+    }
+
+    if (tile.Z < neighbor.Z) {
+      return Direction.EAST;
+    }
     if (tile.Z > neighbor.Z) {
       return Direction.WEST;
     }
@@ -149,12 +158,17 @@ public class WCFGenerator : MonoBehaviour {
   private Tile[] GetNeighbors(Tile tile) {
     List<Tile> neighbors = new List<Tile>();
     for (int x = tile.X - 1; x <= tile.X + 1; ++x) {
-      for (int y = tile.Z - 1; y <= tile.Z + 1; ++y) {
-        if (((x == tile.X && y != tile.Z) || (x != tile.X && y == tile.Z))
-            && (x >= 0 && x < width_)
-            && (y >= 0 && y < height_)
+      for (int y = tile.Y - 1; y <= tile.Y + 1; ++y) {
+        for (int z = tile.Z - 1; z <= tile.Z + 1; ++z) {
+          if (((x == tile.X && y == tile.Y && z != tile.Z)         
+              ||(x == tile.X && y != tile.Y && z == tile.Z)
+              ||(x != tile.X && y == tile.Y && z == tile.Z))
+              && (x >= 0 && x < width_)
+              && (y >= 0 && y < height_)
+              && (z >= 0 && z < depth_)
           ) {
-          neighbors.Add(wave_[x, y]);
+            neighbors.Add(wave_[x, y, z]);
+          }
         }
       }
     }
