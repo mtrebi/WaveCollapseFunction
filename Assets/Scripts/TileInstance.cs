ed8c406d588650c42da02c5612aff5fc5f3f43fa
Mtrebi <marianotrebino@gmail.com>
2017-08-04 12:29:08 +0200

    Place holder for tile models that can be instanciated with different rotation to implement symmetries

---
+++ Assets/Scripts/TileInstance.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInstance {
+  private GameObject prefab_;
+  private Quaternion prefab_orientation_;
+  private float probability_;
+  [SerializeField] private TileAdjacencies adjacencies_;
+
+
+  public TileInstance(GameObject prefab, Quaternion rotation, float probability, TileAdjacencies adjacencies) {
+    prefab_ = prefab;
+    prefab_orientation_ = rotation;
+    probability_ = probability;
+    adjacencies_ = adjacencies;
+  }
+
+
+  // Use this for initialization
+  void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
