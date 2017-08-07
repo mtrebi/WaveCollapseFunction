50101b5917700bbd1434f494ecebe50a3988f905
Mtrebi <marianotrebino@gmail.com>
2017-08-07 13:21:25 +0200

    Added maths utils class

---
+++ Assets/Scripts/Utils/Mathematics.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mathematics {
+
+  public static Vector3 RotateAroundPoint(Vector3 point, Vector3 pivot, Vector3 euler) {
+    return Quaternion.Euler(euler) * (point - pivot) + pivot;
+  }
+}
