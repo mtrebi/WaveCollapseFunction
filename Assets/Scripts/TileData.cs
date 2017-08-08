5ffca33c9223ad3c4177ef47f41d1b69ad34f49c
Mtrebi <marianotrebino@gmail.com>
2017-08-08 12:49:13 +0200

    Rename to TileData

---
+++ Assets/Scripts/TileData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData : MonoBehaviour {
+  public float Probability;
+  public SymmetryType Symmetry;
+}
