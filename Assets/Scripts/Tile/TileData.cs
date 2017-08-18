eef2d5bcce14580fc2c60b25a556dbc741ee25f0
Mtrebi <marianotrebino@gmail.com>
2017-08-18 12:39:26 +0200

    Added TileType to differentiate Empty, Ground, Roof and Mid tiles

--- Assets/Scripts/Tile/TileData.cs
+++ Assets/Scripts/Tile/TileData.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum TileType {
+  GROUND,
+  WALL,
+  ROOF,
+  EMPTY
+}
+
 public class TileData : MonoBehaviour {
   public float Probability;
   public SymmetryType Symmetry;
+  public TileType Type;
 }
