dbbfbcdebcd6738407cb98881a723a4675f39248
Mtrebi <marianotrebino@gmail.com>
2017-09-08 15:56:58 +0200

    Created interactive UI to generate buildings of different sizes

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class UIManager : MonoBehaviour {
   // TODO Interactive UI to build tiles by clicking!
