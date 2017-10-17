cb81f089437af436a5c788a7e4ecb3acbbb50cd3
Mtrebi <marianotrebino@gmail.com>
2017-10-17 11:41:38 +0200

    Created custom Contains method as a StringExtension

---
+++ Assets/Scripts/Utils/StringExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class StringExtensionMethods {
+  public static bool Contains(this string source, string toCheck, System.StringComparison comp) {
+    return source != null && toCheck != null && source.IndexOf(toCheck, comp) >= 0;
+  }
+}
+
