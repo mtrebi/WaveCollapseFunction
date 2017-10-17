e0042cbfd3ad4537a0e638bfbacd0adafa04bc16
Mtrebi <marianotrebino@gmail.com>
2017-10-17 11:38:32 +0200

    Added ROOF type and TODO

--- Assets/Scripts/Tile/TileData.cs
+++ Assets/Scripts/Tile/TileData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public enum Type {
   GROUND,
+  ROOF,
   EMPTY,
   OTHER
 }
@@ -18,4 +19,23 @@ public class TileData : MonoBehaviour {
   public float Probability;
   public SymmetryType Symmetry;
   public Type Type;
+
+
+  private void OnEnable() {
+    SetType();
+  }
+
+  private void SetType() {
+    /* TODO
+    string n = name;
+    if (name.Contains(Type.ROOF.ToString(), System.StringComparison.OrdinalIgnoreCase)) {
+      Type = Type.ROOF;
+    }else if (name.Contains(Type.GROUND.ToString(), System.StringComparison.OrdinalIgnoreCase)) {
+      Type = Type.GROUND;
+    }else if (name.Contains(Type.EMPTY.ToString(), System.StringComparison.OrdinalIgnoreCase)) {
+      Type = Type.EMPTY;
+    }else {
+      Type = Type.OTHER;
+    }*/
+  }
 }
