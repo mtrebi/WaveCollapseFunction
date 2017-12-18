b6a8c84d714a6f1da197a7f813ede8ef47639622
Mtrebi <marianotrebino@gmail.com>
2017-12-18 20:56:34 +0100

    Improved rendering to use different colors for different graphs components

--- Assets/Prefabs/Debug/Renderers/GraphRenderer.cs
+++ Assets/Prefabs/Debug/Renderers/GraphRenderer.cs
@@ -6,12 +6,38 @@ public class GraphRenderer : MonoBehaviour {
   #region Public Fields
   public WCFGenerator generator;
   public GameObject VertexPrefab;
-  public GameObject EdgePrefab;
+  public GameObject EdgePrefab;  
   #endregion
 
   #region Private Fields
   Graph<Tile> graph_;
   Dictionary<Vector3, GameObject> rendered_objects_;
+
+  private Color32[] colors_ = new Color32[]{
+    new Color32(230, 25, 75, 255),
+    new Color32(60, 180, 75, 255),
+    new Color32(255, 225, 25, 255),
+    new Color32(0, 130, 200, 255),
+    new Color32(245, 130, 48, 255),
+    new Color32(145, 30, 180, 255),
+    new Color32(70, 240, 240, 255),
+    new Color32(240, 50, 230, 255),
+    new Color32(210, 245, 60, 255),
+    new Color32(250, 190, 190, 255),
+    new Color32(0, 128, 128, 255),
+    new Color32(230, 190, 255, 255),
+    new Color32(170, 110, 40, 255),
+    new Color32(255, 250, 200, 255),
+    new Color32(128, 0, 0, 255),
+    new Color32(170, 255, 195, 255),
+    new Color32(128, 128, 0, 255),
+    new Color32(255, 215, 180, 255),
+    new Color32(0, 0, 128, 255),
+    new Color32(128, 128, 128, 255),
+    new Color32(255, 255, 255, 255),
+    new Color32(0, 0, 0, 255),
+  };
+
   #endregion
 
   #region Getters/Setters
@@ -67,6 +93,7 @@ public class GraphRenderer : MonoBehaviour {
     Vector3 position = GetPosition(vertex);
     GameObject obj = Instantiate(VertexPrefab, position, Quaternion.identity, this.transform);
     obj.layer = LayerMask.NameToLayer("Debug");
+    ChangeObjColor(obj, CalculateColorComponent(vertex.component_id));
     rendered_objects_.Add(position, obj);
   }
 
@@ -80,11 +107,13 @@ public class GraphRenderer : MonoBehaviour {
 
     GameObject obj = Instantiate(EdgePrefab, position, EdgeRotation(edge), this.transform);
     obj.layer = LayerMask.NameToLayer("Debug");
+    ChangeObjColor(obj, CalculateColorComponent(edge.node1.component_id));
     rendered_objects_.Add(position, obj);
   }
 
   private void DeleteEdge(Edge<Tile> edge) {
-
+    // TODO Delete edge
+    throw new System.NotImplementedException();
   }
 
   private Vector3 GetPosition(Vertex<Tile> vertex) {
@@ -121,6 +150,21 @@ public class GraphRenderer : MonoBehaviour {
     }
 
     return Quaternion.identity;
+  }
+
+  /// <summary>
+  /// Calculate Graph Component color based on component id
+  /// </summary>
+  /// <param name="component_id"></param>
+  /// <returns></returns>
+  private Color32 CalculateColorComponent(int component_id) {
+    return colors_[component_id % colors_.Length];
+  }
+
+  private void ChangeObjColor(GameObject obj, Color32 color) {
+    Renderer rend = obj.GetComponent<Renderer>();
+    rend.material.shader = Shader.Find("Unlit/Color");
+    rend.material.color = color;
   }
 
   #endregion
