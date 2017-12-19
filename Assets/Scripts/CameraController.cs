da9c9a1e96f08454052573e5fac7bca8be85da74
Mtrebi <marianotrebino@gmail.com>
2017-12-19 11:53:56 +0100

    Created Camera Controller to manage interaction with camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+
+public class CameraController : MonoBehaviour {
+  #region Public Fields
+  public const int min_zoom = 4;
+  public const int max_zoom = 7;
+  #endregion
+
+  #region Private Fields
+  private float zoom_ = 0;
+  public GameObject main_camera;
+  public GameObject debug_camera;
+  #endregion
+
+  #region Getters/Setters
+
+  #endregion
+
+  #region Unity Methods
+  private void Awake() {
+    main_camera = this.gameObject;
+    debug_camera = this.transform.FindChild("Debug Camera").gameObject;
+  }
+
+  private void Start() {
+      
+  }
+
+  private void Update(){
+    UpdateZoomFromMouseWheel();
+  }
+  #endregion
+
+  #region Private Methods
+  private void UpdateZoomFromMouseWheel() {
+    zoom_ -= Input.GetAxis("Mouse ScrollWheel");
+    zoom_ = Mathf.Clamp(zoom_, min_zoom, max_zoom);
+    main_camera.GetComponent<Camera>().orthographicSize = zoom_;
+    debug_camera.GetComponent<Camera>().orthographicSize = zoom_;
+  }
+
+  /*
+    private void RotateCameraAround() {
+    if (Input.GetMouseButtonDown(0)) {
+      Vector3 rotation_point = new Vector3(width_, 0, depth_) / 2;
+      camera_yaw_ += camera_rotation_speed * Input.GetAxis("Mouse X");
+      main_camera.transform.RotateAround(rotation_point, Vector3.up, camera_yaw_ * Time.deltaTime);
+    }
+  }
+  */
+  #endregion
+
+}
+ 
