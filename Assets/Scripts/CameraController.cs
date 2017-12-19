f7b1b20e836f5374739307b8d95b536f7caefefc
Mtrebi <marianotrebino@gmail.com>
2017-12-19 12:21:18 +0100

    Implemented Camera Rotation around object

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,17 +3,27 @@
 
 public class CameraController : MonoBehaviour {
   #region Public Fields
+  public const float speed = 2.0f;
   public const int min_zoom = 4;
   public const int max_zoom = 7;
   #endregion
 
   #region Private Fields
-  private float zoom_ = 0;
   public GameObject main_camera;
   public GameObject debug_camera;
+
+  private Vector3 rotation_point_ = Vector3.zero;
+  private float zoom_ = 0;
+  private bool left_click_down_ = false;
   #endregion
 
   #region Getters/Setters
+
+  public Vector3 RotationPoint {
+    set {
+      rotation_point_ = value;
+    }
+  }
 
   #endregion
 
@@ -21,6 +31,7 @@ public class CameraController : MonoBehaviour {
   private void Awake() {
     main_camera = this.gameObject;
     debug_camera = this.transform.FindChild("Debug Camera").gameObject;
+    // rotation_point is assigned from the outside everytime a new building is generated
   }
 
   private void Start() {
@@ -28,27 +39,32 @@ public class CameraController : MonoBehaviour {
   }
 
   private void Update(){
-    UpdateZoomFromMouseWheel();
+    CameraRotation();
+    CameraZoom();
   }
   #endregion
 
   #region Private Methods
-  private void UpdateZoomFromMouseWheel() {
+  private void CameraZoom() {
     zoom_ -= Input.GetAxis("Mouse ScrollWheel");
     zoom_ = Mathf.Clamp(zoom_, min_zoom, max_zoom);
     main_camera.GetComponent<Camera>().orthographicSize = zoom_;
     debug_camera.GetComponent<Camera>().orthographicSize = zoom_;
   }
 
-  /*
-    private void RotateCameraAround() {
+  
+  private void CameraRotation() {
     if (Input.GetMouseButtonDown(0)) {
-      Vector3 rotation_point = new Vector3(width_, 0, depth_) / 2;
-      camera_yaw_ += camera_rotation_speed * Input.GetAxis("Mouse X");
-      main_camera.transform.RotateAround(rotation_point, Vector3.up, camera_yaw_ * Time.deltaTime);
+      left_click_down_ = true;
+    }
+    if (Input.GetMouseButtonUp(0)) {
+      left_click_down_ = false;
+    }
+    if (left_click_down_) {
+      main_camera.transform.RotateAround(rotation_point_, Vector3.up, Input.GetAxis("Mouse X") * speed);
     }
   }
-  */
+  
   #endregion
 
 }
